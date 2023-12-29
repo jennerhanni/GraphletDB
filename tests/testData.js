f12ef14122d27f620dceebfac7239f1aa59da4a5
Jenner Hanni <jenner@jennerhanni.net>
2023-12-28 18:50:50 -0800

    Whitespace

--- tests/testData.js
+++ tests/testData.js
@@ -1,9 +1,33 @@
+const validMinimumNodes = [
+    {   id: 1, 
+        label: 'Label', 
+        date: '202312311822', 
+        strLabel: 'Label',
+        strLabelDescription: 'The basic building block of the database.' },
+];
+
 const validNodes = [
-    { id: 1, label: 'Node1', date: '2023-01-01', strLabel: 'Node1' },
-    { id: 2, label: 'Label', date: '2023-01-02', strLabel: 'Node1' },
-    // ... other nodes
+    {   id: 1, 
+        label: 'Label', 
+        date: '202312311822', 
+        strLabel: 'Label',
+        strLabelDescription: 'The basic building block of the database.' },
+    {   id: 1, 
+        label: 'Label', 
+        date: '202312311822', 
+        strLabel: 'Topic',
+        strLabelDescription: 'A tag, keyword, topic, subject, person, place, etc by which other nodes can be linked.',
+        strText: '' },
+    {   id: 1, 
+        label: 'Topic', 
+        date: '202312311822', 
+        strLabel: 'Label',
+        strLabelDescription: 'The basic building block of the database.',
+        strText: 'History' 
+    },
 ];
 
 module.exports = {
+    validMinimumNodes,
     validNodes,
 }
