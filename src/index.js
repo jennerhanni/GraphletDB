6569814fe7049965286b97014ddc22e590eb9c78
Jenner Hanni <jenner@jennerhanni.net>
2024-01-28 09:51:13 -0800

    [test] bugfixes

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ function getTypeStr(fullTypeString) {
 
 // this expects @graph
 // generate a random lowercase hexstring
-function getRandomToken(nodes, len) {   
+function getRandomToken(nodesDict, len) {   
     let token;
     let isUnique;
     let count = 0;
@@ -38,7 +38,7 @@ function getRandomToken(nodes, len) {
         }
 
         // check if token is unique
-        let existingNodesList = listFoundKeyVal(nodes, "id", token)
+        let existingNodesList = listFoundKeyVal(nodesDict, null, "id", token, true)
         if (existingNodesList.length === 0) {
             isUnique = true
         }
@@ -56,12 +56,18 @@ function getRandomToken(nodes, len) {
 // this accepts not the whole dbJson but the subset of nodes to be examined
 // return a list of entries. if FIRSTONLY is true, 
 // return only the first node encountered.
-function listFoundKeyVal(nodes, key, value, firstOnly=false) {
+// TODO: add a type arg to make the lookup faster by limiting to typecollection
+function listFoundKeyVal(nodesDict, targetType, key, value, firstOnly=false) {
+    console.log('listFoundKeyVal', targetType, key, value, firstOnly)
     try {
+        if (typeof nodesDict !== "object" || Array.isArray(nodesDict)) {
+            throw new Error("Invalid or inappropriate @graph structure");
+        }
+
         // find any (firstOnly = true) or all (firstOnly = false) matching entries
         let entriesToReturn = [];
-        for (let typeCollectionKey in nodes["@graph"]) {
-            let typeCollection = nodes["@graph"][typeCollectionKey];
+        for (let typeCollectionKey in nodesDict) {
+            let typeCollection = nodesDict[typeCollectionKey];
             if (typeCollection["@type"] === "TypeCollection" 
                 && Array.isArray(typeCollection["gjs:entries"])) {
                 for (let entry of typeCollection["gjs:entries"]) {
@@ -86,7 +92,10 @@ function listFoundKeyVal(nodes, key, value, firstOnly=false) {
 // get a template object out of the templates array
 function getTemplateObj(templateNodes, key, val) {
     try {
-        return Object.assign({}, templateNodes.find(n => n[key] === val) || null)
+        console.log('getTemplateObj *****', key, val, templateNodes.find(n => n[key] === val))
+        let nodeToReturn;
+        nodeToReturn = Object.assign({}, templateNodes.find(n => n[key] === val) || null)
+        return nodeToReturn;
     } catch (e) {
         console.log("getTemplateObj failed, template not found for key, val:", key, val);
         throw new Error('template object not found'); 
@@ -95,12 +104,12 @@ function getTemplateObj(templateNodes, key, val) {
 
 
 // return a list of all the type collection strings in @graph
-function getListOfTypeCollections(db) {
+function getListOfTypeCollections(nodesDict) {
     try {
-        if (!db["@graph"] || typeof db["@graph"] !== "object" || Array.isArray(db["@graph"])) {
+        if (typeof nodesDict !== "object" || Array.isArray(nodesDict)) {
             throw new Error("Invalid or inappropriate @graph structure");
         }
-        return Object.keys(db["@graph"]);
+        return Object.keys(nodesDict);
     } catch (error) {
         console.error("Error in getListOfTypeCollections:", error.message);
         throw error; 
@@ -109,14 +118,14 @@ function getListOfTypeCollections(db) {
 
 // NEEDS TRIPLE CHECK, SOMETHING IS VERY WEIRD
 // WHAT IS THIS FUNCTION FOR?
-// return a list of all keys in a list of nodes
-function getListOfKeys(nodes) {
+// return a list of all keys in an @graph dict
+function getListOfKeys(nodesDict) {
     console.log("getListOfKeys");
 
     let keySet = new Set();
 
     // check every TypeCollection's nodes in 'entries' to see what keys are present 
-    Object.values(nodes['@graph']).forEach(typeCollection => {
+    Object.values(nodesDict).forEach(typeCollection => {
         if (typeCollection['gjs:entries'] && Array.isArray(typeCollection['gjs:entries'])) {
             typeCollection['gjs:entries'].forEach(node => {
                 Object.keys(node).forEach(key => {
@@ -169,8 +178,9 @@ const getDateObjects = () => {
 
 
 // return a list of the rel__-prefixed props
+// TODO: test this!! should work on the rel__prop itself and disregard the "context:"
 function hasRelProps(nodeToAdd) {
-    return Object.keys(nodeToAdd).filter(key => key.startsWith("archively:rel__"));
+    return Object.keys(nodeToAdd).filter(key => getTypeStr(key).startsWith("rel__"));
 }
 
 
@@ -185,7 +195,9 @@ function initTypeCollection(dbJson, typeStr) {
        // grab the @type template for the core template
        let newTypeCollection = getTemplateObj(dbJson['templates'], "@type", "gjs:TypeCollection");
        newTypeCollection['gjs:typeName'] = typeStr;
+       console.log('newTC', newTypeCollection)
        dbJson['@graph'][typeStr] = newTypeCollection;
+       console.log("@GRAPH", dbJson['@graph'])
     } catch (err) {
         console.error(err);
     }    
@@ -218,23 +230,37 @@ function initNode(dbJson, nodeTypeStr, strUserSlug='default') {
 
 
 // add a new node to @graph
+// if the node exists, this will not write but return a warning
+// this function does not update target nodes to ensure bidirectionality
 function addNode(dbJson, nodeToAdd) {
-    console.log("addNode", nodeToAdd)
+    console.log("addNode nodeToAdd", nodeToAdd)
 
     nodeToAdd = Object.assign({}, nodeToAdd)
     try {
         // identify the type 
         let typeStr = getTypeStr(nodeToAdd['@type'])
-        console.log('addNode typeStr', nodeToAdd, typeStr)
+        console.log('addNode typeStr', typeStr)
 
         // create the typecollection if it does not exist
-        let typeCollectionList = getListOfTypeCollections(dbJson)
+        // and add the node to the TypeCollection
+        let typeCollectionList = getListOfTypeCollections(dbJson['@graph'])
+        console.log('addNode typeCollectionList', typeCollectionList)
         if (!typeCollectionList.includes(nodeToAdd['@type'])) {
             initTypeCollection(dbJson, typeStr)
+            dbJson['@graph'][typeStr]['gjs:entries'] = [nodeToAdd]
+        } else {
+            dbJson['@graph'][typeStr]['gjs:entries'].push(nodeToAdd)
         }
+        console.log('addNode after initTypeCollection @GRAPH', dbJson['@graph'])
 
-/** skip this for now, we're handling it in the script
-        // handle bidirectionality if there are any rel-prefixed nodes
+        
+    } catch (err) {
+        console.error(err)
+        throw err;
+    }
+}
+
+/**        // handle bidirectionality if there are any rel-prefixed nodes
         let relPropsList = hasRelProps(nodeToAdd); 
         if (relPropsList.length > 0) {
             console.log('relPropsList is nonzero', relPropsList);
@@ -244,13 +270,29 @@ function addNode(dbJson, nodeToAdd) {
                     console.log(`Processing non-empty array for property: ${prop}`);
                     propArray.forEach(id => {
                         console.log(`Processing id: ${id} in property: ${prop}`);
+                        updateTargetNode(dbJson['@graph'], null, id, nodeToAdd['gjs:@id'], prop, 'ADD')
                     });
                 }
             });
-        } **/
+        } 
+**/
 
-        // add this node to the TypeCollection
-        dbJson['@graph'][typeStr]['gjs:entries'].push(nodeToAdd)
+// enforce bidirectionality by ADD or REMOVE a sourceId from a target node's appropriate rel__ prop
+function updateTargetNode(nodesDict, targetType='archively:Placeholder', targetId, sourceId, 
+                          relKey='gjs:rel__nodes', op) {
+    console.log('updateTargetNode', nodesDict, targetType, targetId, sourceId, relKey, op)
+
+    try {
+        // find the target node in nodesDict
+        let foundNodesList = listFoundKeyVal(nodesDict, targetType, 'gjs:@id', targetId, true)
+
+        // if the node does not exist, create it 
+        if (!foundNodesList || foundNodesList.length === 0) {
+            
+        }
+
+        console.log('foundNodesList', foundNodesList)
+
     } catch (err) {
         console.error(err)
         throw err;
